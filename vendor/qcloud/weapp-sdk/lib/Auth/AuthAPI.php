29b0ce8072aa6e6405a2f0887e03b3f1a6dc9f69
lsky <clin520@126.com>
2018-04-24 17:26:20 +0800

    modify qcloud api

--- vendor/qcloud/weapp-sdk/lib/Auth/AuthAPI.php
+++ vendor/qcloud/weapp-sdk/lib/Auth/AuthAPI.php
@@ -43,28 +43,10 @@ class AuthAPI {
         // 4. 储存到数据库中
         User::storeUserInfo($userinfo, $skey, $sessionKey);
 
-        // 本人登录
-        if($userinfo->openId === '111'){
-            return [
-                'loginState' => Constants::S_AUTH,
-                'userinfo' => compact('userinfo', 'skey')
-            ];
-        }else if($userinfo->openId == 'opIzq4p4OSlQIQ_Ia9EExwCuQJXs'){
-            // 宝贝登录
-            return [
-                'loginState' => Constants::S_AUTH,
-                'userinfo' => compact('userinfo', 'skey')
-            ];
-        }else{
-            return [
-                'loginState' => Constants::E_AUTH,
-                'error' => '您暂无权限登录该小程序'
-            ];
-        }
-        // return [
-        //     'loginState' => Constants::S_AUTH,
-        //     'userinfo' => compact('userinfo', 'skey')
-        // ];
+        return [
+            'loginState' => Constants::S_AUTH,
+            'userinfo' => compact('userinfo', 'skey')
+        ];
     }
 
     public static function checkLogin($skey) {
